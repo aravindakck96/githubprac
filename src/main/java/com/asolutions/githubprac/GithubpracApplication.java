197c56aa7a4debddf116be7bc9c58d17c878ed3e
aravindakck96 <158430927+aravindakck96@users.noreply.github.com>
2025-01-03 08:59:35 +0530

    Initial commit

---
+++ src/main/java/com/asolutions/githubprac/GithubpracApplication.java
@@ -0,0 +1,13 @@
+package com.asolutions.githubprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubpracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asolutions/githubprac/GithubpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asolutions.githubprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
