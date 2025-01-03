75be4b96c7f9eae74d9e0295ddffa9ace7e3da1b
aravindakck96 <158430927+aravindakck96@users.noreply.github.com>
2025-01-03 09:30:16 +0530

    entity

---
+++ src/main/java/com/asolutions/githubprac/model/Client.java
@@ -0,0 +1,32 @@
+package com.asolutions.githubprac.model;
+
+public class Client {
+
+private int numer;
+private String name;
+private String age;
+
+    public int getNumer() {
+        return numer;
+    }
+
+    public void setNumer(int numer) {
+        this.numer = numer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
